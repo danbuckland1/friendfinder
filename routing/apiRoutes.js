b1734d08f3118a446732210784249d5ffb93f883
danbuckland1 <buckd409@gmail.com>
2018-08-21 15:18:08 -0400

    More JQuery in survey html to accept user input.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,9 +1,13 @@
-var friend = require("../app/data/friends");
+var friendData = require("../app/data/friends");
 
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res){
-        res.json(friend);
+        res.json(friendData);
     });
-}
+};
 
+    app.post("/api/friends", function(req, res){
+        
+      });
+  
