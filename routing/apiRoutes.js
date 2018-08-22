81393ad73b23e93b12225c4ce70612f59a0d1e4e
danbuckland1 <buckd409@gmail.com>
2018-08-22 15:02:48 -0400

    Finishing up logic in routes and server files.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,13 +1,37 @@
-var friendData = require("../app/data/friends");
+var friendArray = require("../app/data/friends");
 
 module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friendArray);
+  });
 
-    app.get("/api/friends", function(req, res){
-        res.json(friendData);
-    });
+  app.post("/api/friends", function(req, res) {
+    var userData = req.body.scores;
+    var scoreArray = [];
+    var friendCount = 0;
+    var friendMatch = 0;
+
+    for (var i = 0; i < friendArray.length; i++) {
+      var totalDiff = 0;
+
+      for (var j = 0; j < userData.length; j++) {
+        totalDiff += Math.abs(
+          parseInt(friendArray[i].scores[j]) - parseInt(userData[j])
+        );
+      }
+
+      scoreArray.push(totalDiff);
+    }
+
+    for (var i = 0; i < scoreArray.length; i++) {
+      if (scoreArray[i] <= scoreArray[friendMatch]) {
+        friendMatch = i;
+      }
+    }
+
+    var newFriend = friendArray[friendMatch];
+    res.join(newFriend);
+
+    friendArray.push(req.body);
+  });
 };
-
-    app.post("/api/friends", function(req, res){
-        
-      });
-  
